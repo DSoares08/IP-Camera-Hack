010f062157707fa218f933f7381fc44b53e624c2
Diogo Soares <diogo.soares05@gmail.com>
2025-12-05 11:31:49 +0100

    added ip and mac addresses of network devices for arp spoofing

---
+++ arp_spoofing.py
@@ -0,0 +1,14 @@
+from scapy.all import *
+import time
+
+camera_ip = "192.168.0.81"
+camera_mac = "3C:64:CF:5D:C1:F6"
+
+router_ip = "192.168.0.1"
+router_mac = "5C:62:8B:57:93:4C"
+
+display_device_ip = "192.168.0.162"
+display_device_mac = "58:1C:F8:51:BD:3C"
+
+attacker_ip = "192.168.0.150"
+attacker_mac = "46:4C:D7:C3:7F:F1"
