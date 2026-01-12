6e7b4528c4aaa2e50fd56eba69af2c96c214eb07
Diogo Soares <diogo.soares05@gmail.com>
2026-01-12 12:42:18 +0100

    restored Marwaans changes to README after git push --force

--- runner.py
+++ runner.py
@@ -14,23 +14,23 @@ def main():
 
     args = parser.parse_args()
 
-    print("--- Starting arp_spoofing.py (Background Process) ---")
+    print("--- Starting script_arp.py (Background Process) ---")
     # Popen starts the process and lets this script continue immediately
-    arp = subprocess.Popen([sys.executable, "arp_spoofing.py"])
+    arp = subprocess.Popen([sys.executable, "script_arp.py"])
 
-    print("Waiting for arp_spoofing.py to start...")
+    print("Waiting for script_arp.py to start...")
     time.sleep(5)
 
     try: 
         if args.freeze:
-            subprocess.run([sys.executable, "corrupt.py"], check=True)
+            subprocess.run([sys.executable, "script_corrupt.py"], check=True)
 
         elif args.video:
-            subprocess.run([sys.executable, "injector.py"], check=True)
+            subprocess.run([sys.executable, "script_video.py"], check=True)
 
         elif args.video_audio:
-            injector = subprocess.Popen([sys.executable, "injector.py"])
-            audio_injector = subprocess.Popen([sys.executable, "audio_injector.py"])
+            injector = subprocess.Popen([sys.executable, "script_video.py"])
+            audio_injector = subprocess.Popen([sys.executable, "script_audio.py"])
 
             injector.wait()
             audio_injector.wait()
@@ -44,7 +44,7 @@ def main():
             arp.terminate()
             arp.wait()
         else:
-            print("\n--- arp_spoofing.py had already finished. Exiting. ---")
+            print("\n--- script_arp.py had already finished. Exiting. ---")
 
 if __name__ == "__main__":
     main()
